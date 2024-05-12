b2fc333a1bf25f314cd5b26abea3a0eeea79ea53
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-05-12 10:52:11 +0200

    work on header and materialUI

--- ecom-frontend/src/components/header.tsx
+++ ecom-frontend/src/components/header.tsx
@@ -1,86 +1,112 @@
-import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import { Stack } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
+import SearchIcon from "@mui/icons-material/Search";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import { Link } from "react-router-dom";
 
-const pages = ["Products", "Pricing", "Blog"];
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
-function Header() {
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static">
         <Toolbar>
+          <Stack
+            direction="row"
+            spacing={3}
+            sx={{ flexGrow: 2, display: { xs: "none", md: "block" } }}
+          >
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/product">
+              All products
+            </Button>
+            <Button color="inherit" component={Link} to="/contact">
+              Contact us!
+            </Button>
+          </Stack>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 2, justifyContent: "center" }}
+          >
+            MUI
+          </Typography>
+          <Search sx={{ flexGrow: 1, display: { xs: "none", md: "block" } }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            sx={{
+              display: {
+                xs: "block",
+                md: "none",
+                justifyContent: "flex - end",
+              },
+            }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
-export default Header;
-
-// import { Link } from "react-router-dom"; // Since you are already using React Router
-// import Toolbar from "@mui/material/Toolbar"; // To hold navigation and other content in the AppBar
-// import Typography from "@mui/material/Typography"; // For inserting text
-// import Button from "@mui/material/Button"; // To use buttons for navigation links
-// import IconButton from "@mui/material/IconButton"; // For icons within buttons
-// import MenuIcon from "@mui/icons-material/Menu"; // Example icon
-// import SearchIcon from "@mui/icons-material/Search"; // Example icon
-// import { styled } from "@mui/material/styles"; // Utility for styling Material UI components
-// import { Icon } from "@mui/material";
-
-// // Temporary variables
-// const title = null;
-
-// function Header() {
-//   return (
-//     <header>
-//       <>
-//         <Toolbar>
-//           <nav noWrap sx={{ flex: 1 }}>
-//             <Link to="/">Home</Link>
-//             <Link to="/product">All products</Link>
-//             <Link to="/contact">Contact us!</Link>
-//           </nav>
-//           <Typography
-//             component="h1"
-//             variant="h3"
-//             color="inherit"
-//             align="center"
-//             noWrap
-//             sx={{ flex: 2 }}
-//           >
-//             {title || "Brilliant buys"}
-//           </Typography>
-//           <IconButton sx={{ flex: 1 }}>
-//             <SearchIcon />
-//           </IconButton>
-//           <IconButton></IconButton>
-//         </Toolbar>
-//       </>
-//     </header>
-//   );
-// }
-
-// export default Header;
