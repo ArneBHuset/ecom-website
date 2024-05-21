74cfd70c39e81c847cc8c95b365fb264eefcccd7
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-05-21 15:11:33 +0200

    Revert

--- ecom-frontend/src/components/common/ProductItem.tsx
+++ ecom-frontend/src/components/common/ProductItem.tsx
@@ -13,8 +13,12 @@ import StarIcon from "@mui/icons-material/Star";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+<<<<<<< HEAD
 // import ApiCall from "../../api/api-call";
 import { Box } from "@mui/material";
+=======
+import ApiCall from "../../api/api-call";
+>>>>>>> parent of 9c612be (work on API handeling and JSX population)
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -47,6 +51,19 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
 export default function ProductItem({ product }: any) {
   const [expanded, setExpanded] = useState(false);
 
+<<<<<<< HEAD
+=======
+  useEffect(() => {
+    const loadData = async () => {
+      const apiData = await ApiCall();
+      setData(apiData);
+    };
+    loadData();
+  }, []);
+
+  console.log(jsonData);
+
+>>>>>>> parent of 9c612be (work on API handeling and JSX population)
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
@@ -60,6 +77,7 @@ export default function ProductItem({ product }: any) {
   }
 
   return (
+<<<<<<< HEAD
     <Box>
       {product &&
         product.data.map((product: any) => (
@@ -125,5 +143,68 @@ export default function ProductItem({ product }: any) {
           </Card>
         ))}
     </Box>
+=======
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Product example"
+        subheader="Prise of product"
+        // subheader="Reduced price of product"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://static.noroff.dev/api/online-shop/1-perfume-white.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <AddShoppingCartIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <StarIcon />
+        </IconButton>
+        <Typography>This is the rating</Typography>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <IconButton aria-label="add to favorites">
+        <TagIcon />
+      </IconButton>
+      <Typography>Tags and such</Typography>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          {
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              RI
+            </Avatar>
+          }
+          <Typography paragraph>Username of reviewer</Typography>
+          <Typography paragraph>This is the reviewers description</Typography>
+          <IconButton aria-label="share">
+            <StarIcon />
+          </IconButton>
+          <Typography>This is the rating</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+>>>>>>> parent of 9c612be (work on API handeling and JSX population)
   );
 }
--- ecom-frontend/src/pages/LandingPage.tsx
+++ ecom-frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,8 @@
 import { Container } from "@mui/material";
 import Header from "../components/common/Header";
-import HeroSection from "../components/layout/Hero";
 
 function LandingPage() {
+  ApiCall();
   return (
     <>
       <Container>
