6644d7e0d6de1e560bae30f4a1547320f33d4bb7
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-06-07 19:56:54 +0200

    test Netlify

--- ecom-frontend/src/main.tsx
+++ ecom-frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@mui/material";
-import { CartProvider } from "./components/ui/cart/CartContext.tsx";
-import theme from "./styles/theme.ts";
+import { CartProvider } from "./components/ui/cart/CartContext";
+import theme from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
