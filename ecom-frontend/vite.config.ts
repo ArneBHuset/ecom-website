77bd4791cebf0eed3303d1d26e8ccfd5a690e435
ArneBHuset <128044224+ArneBHuset@users.noreply.github.com>
2024-06-07 21:01:55 +0200

    Netlify test

--- ecom-frontend/vite.config.ts
+++ ecom-frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ecom-frontend/", // Use '/ecom-frontend/' if deploying under a subdirectory
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "public/index.html", // Explicitly set input to avoid confusion
+    },
+  },
+});
